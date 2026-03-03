0b60af82d58f029e7da854c0494a823f2c9445a5
maha2225 <mahasebri24@gmail.com>
2026-03-04 01:04:24 +0300

    Serverless auth added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js"; 
-import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import moduleRoutes from "./routes/module.routes.js";
 import enrollmentRoutes from "./routes/enrollment.routes.js";
@@ -20,7 +19,6 @@ const swaggerDocument = YAML.load('./docs/swagger.yaml');
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/modules", moduleRoutes);
 app.use("/api/enrollments", enrollmentRoutes);
